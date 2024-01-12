d6ddbacb457837bb59e1e779f7ae89f52f546889
Kyumin Lee <kmkm0903@naver.com>
2024-01-12 20:02:09 +0900

    feat: 초기

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
-function App() {
-  // eslint-disable-next-line react/react-in-jsx-scope, react/jsx-filename-extension
-  return <div className="App" />;
-}
+import React from 'react';
+import './index.css'; // Tailwind CSS styles
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Home } from './page';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default App;
